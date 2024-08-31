2dc5d0f0b0d722c027eb5fbf933bf3fd4e9650c4
gptahmed1 <gptahmed1@gmail.com>
2024-08-31 21:08:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
 GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY')
-KOYEB_APP_URL = os.environ.get('KOYEB_APP_URL') # أضف هذا السطر
+KOYEB_APP_URL = os.environ.get('KOYEB_APP_URL') 
