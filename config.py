f93cc7275debb2bd96f0f9ac8964557efc576739
gnralo <145225135+gnralo@users.noreply.github.com>
2024-10-11 23:52:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
-GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY')
-KOYEB_APP_URL = os.environ.get('KOYEB_APP_URL') 
+TELEGRAM_BOT_TOKEN = os.environ.get('7482440969:AAHqK1-CEIqfHgidmbvQbUs_eWh0QGSR22s')
+GEMINI_API_KEY = os.environ.get('AIzaSyA5pzOpKVcMGm6Aek82KoB3Pk94dYg3LX4')
+KOYEB_APP_URL = os.environ.get('') 
