b330aca8584c49a6fd9e255c0e978d05b33d1312
gnralo <145225135+gnralo@users.noreply.github.com>
2024-10-11 23:59:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 TELEGRAM_BOT_TOKEN = os.environ.get('7482440969:AAHqK1-CEIqfHgidmbvQbUs_eWh0QGSR22s')
 GEMINI_API_KEY = os.environ.get('AIzaSyA5pzOpKVcMGm6Aek82KoB3Pk94dYg3LX4')
-KOYEB_APP_URL = os.environ.get('') 
+KOYEB_APP_URL = os.environ.get('https://gimini1u.onrender.com') 
